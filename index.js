4a1c45f2f8a066a74f7d6ec9a9fe8c0178593650
Asad <axsad398@gmail.com>
2023-07-03 22:30:24 +0500

    adding cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const PORT = process.env.PORT || 4000;
+const cors = require('cors');
 
 // mongoose is used to create Schemas and Database for the application, it is a ODM, Object Data Modelling
 // Like you can Create a schema that for every blog post saved in DB there must be a title, author, date, content
@@ -13,13 +14,8 @@ const app = express();
 // middleware code is executes getting responsce from server and sending a response
 // global middleware
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
- })
- 
 app.use(express.json())
+app.use(cors())
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
