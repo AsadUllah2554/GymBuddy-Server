a74fdc6176ab8efa9bcc76a84e4824a598055afd
Asad <axsad398@gmail.com>
2023-07-03 22:22:02 +0500

    adding cors

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ const app = express();
 // middleware code is executes getting responsce from server and sending a response
 // global middleware
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ })
+ 
 app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method);
