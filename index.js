b7dd73d9b5588e05fb69eb6c1dc47a36cb4c61ca
Asad <axsad398@gmail.com>
2023-07-04 16:57:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const userRoutes = require("./routes/user");
 
 const app = express();
 
-app.use(cors({ origin: "https://gymbuddy-two.vercel.app" }));
+app.use(cors());
 
 // middleware code is executes getting responsce from server and sending a response
 // global middleware
